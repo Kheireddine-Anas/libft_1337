53f938154ab5b7526f83cd5bb74cda0a0e9a07fd
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-13 22:55:39 +0100

    FIxed

--- ft_memset.c
+++ ft_memset.c
@@ -17,7 +17,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*dst;
 
 	dst = (unsigned char *)b;
-	while (len-- > 0 && dst)
+	while (len-- > 0)
 		*dst++ = (unsigned char)c;
 	return (b);
 }
