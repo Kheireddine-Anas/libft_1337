3d281578a3147f720ea62c7c1a2a24db6cf367c4
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-13 21:46:39 +0100

    Fix LIBFT-Header

--- libft.h
+++ libft.h
@@ -24,7 +24,7 @@ int		ft_isprint(int c);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
 char	*strdup(const char *s);
