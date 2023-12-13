6ee1ae854c600eae94028eda215b7c4d2e3cb67c
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-13 22:58:55 +0100

    try

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	int	i;
 
 	i = 0;
-	if (dst == NULL || src == NULL)
-		return (NULL);
 	while (n-- > 0)
 	{
 		*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
