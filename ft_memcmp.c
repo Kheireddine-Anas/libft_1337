28d83e2c5ea32eb6490bfd2ee3a0bad3ac467a15
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-15 20:53:20 +0100

    Try 3

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:07:02 by akheired          #+#    #+#             */
-/*   Updated: 2023/12/15 18:07:02 by akheired         ###   ########.fr       */
+/*   Updated: 2023/12/15 19:45:30 by akheired         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*str1;
-	const unsigned char	*str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 	size_t				i;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
+	i = 0;
 	while (i < n)
 	{
 		if ((unsigned char)str1[i] > (unsigned char)str2[i])
