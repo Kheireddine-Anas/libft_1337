dfe8a8b5b32b9b2f0c9013a1e090b13a7d9b1ec8
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-15 18:36:26 +0100

    fix 2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -18,8 +18,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*str2;
 	size_t				i;
 
-	str1 = (const unsigned char *)s1;
-	str2 = (const unsigned char *)s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
 	while (i < n)
@@ -30,5 +30,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			return (-1);
 		i++;
 	}
-	return ((unsigned char)str1[i - 1] - (unsigned char)str2[i - 1]);
+	return (0);
 }
