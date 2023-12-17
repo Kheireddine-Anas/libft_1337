372b8f36578ab23e50f3aeb72b8181129e2cd6aa
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-17 16:50:10 +0100

    Adding STRJOIN Worked

---
+++ ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 15:37:41 by akheired          #+#    #+#             */
+/*   Updated: 2023/12/17 16:45:33 by akheired         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	l_s1;
+	size_t	l_s2;
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	l_s1 = ft_strlen(s1);
+	l_s2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (l_s1 + l_s2) + 1);
+	if (!str)
+		return (NULL);
+	while (i[s1])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 18:09:19 by akheired          #+#    #+#             */
-/*   Updated: 2023/12/16 16:55:36 by akheired         ###   ########.fr       */
+/*   Updated: 2023/12/17 15:38:22 by akheired         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
