aad7bca8d8e3555c1b597b97a4ddeeb03bbffcb8
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-24 08:32:24 +0100

    Completing STRMAPI

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,37 @@
 /*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 18:01:22 by akheired          #+#    #+#             */
-/*   Updated: 2023/12/23 18:01:22 by akheired         ###   ########.fr       */
+/*   Updated: 2023/12/24 08:31:16 by akheired         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	c_word(char const *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	return (i);
+}
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*str;
+	char	*rzlt;
+	size_t	y;
 
+	if (!s)
+		return (NULL);
+	y = 0;
+	str = malloc(sizeof(char) * (c_word(s) + 1));
+	if (!str)
+		return (NULL);
+	rzlt = str;
+	while (*s)
+		*str++ = f(y++, *s++);
+	*str = '\0';
+	return (rzlt);
 }
