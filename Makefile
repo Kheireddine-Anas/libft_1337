bafabebe7b490ebe6a30c81ab917e9d25bc2cd04
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-12-26 13:51:17 +0100

    Adding bonus LSTADD_BACK & LSTLAST & LSTSIZE

--- Makefile
+++ Makefile
@@ -41,6 +41,9 @@ SRCS=	ft_atoi.c\
 
 BSRC=ft_lstnew.c\
 	ft_lstadd_front.c\
+	ft_lstsize.c\
+	ft_lstlast.c\
+	ft_lstadd_back.c\
 
 
 OBJ		= ${SRCS:.c=.o}
---
+++ ft_lstadd_back.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 13:11:34 by akheired          #+#    #+#             */
+/*   Updated: 2023/12/26 13:11:34 by akheired         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*hld;
+
+	if (!lst || !new)
+		return;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	hld = *lst;
+	while (hld->next != NULL)
+	{
+		hld = hld->next;
+	}
+	hld->next = new;
+	
+}
