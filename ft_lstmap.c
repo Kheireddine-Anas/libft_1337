3240862df993bb11cf1d336c4ca972216eb3c29e
Kheireddine-Anas <kheireddine.anas@gmail.com>
2024-01-01 12:42:08 +0100

    Fixing LSTMAP and re-checking it with francinette --strict

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 11:47:53 by akheired          #+#    #+#             */
-/*   Updated: 2024/01/01 12:25:01 by akheired         ###   ########.fr       */
+/*   Updated: 2024/01/01 12:39:59 by akheired         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		n_node = ft_lstnew(set);
 		if (!n_node)
 		{
-			del(lst->content);
+			del(set);
 			ft_lstclear(&n_lst, (*del));
 			return (n_lst);
 		}
