d49e5c54224042ad6e437a61df3f074b4330d37b
Kheireddine-Anas <kheireddine.anas@gmail.com>
2024-01-01 09:59:50 +0100

    Fixing it

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 13:06:02 by akheired          #+#    #+#             */
-/*   Updated: 2023/12/26 13:06:02 by akheired         ###   ########.fr       */
+/*   Updated: 2024/01/01 09:39:10 by akheired         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst != NULL)
-	{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
 		lst = lst->next;
-	}
 	return (lst);
 }
